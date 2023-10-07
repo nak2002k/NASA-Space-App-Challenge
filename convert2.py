1157c248b54dce0acb8e2d917318209f8b555834
Deven1902 <devenamazingnandapurkar@gmail.com>
2023-10-08 01:50:25 +0530

    feature 3

--- convert2.py
+++ convert2.py
@@ -3,7 +3,7 @@ import numpy as np
 import librosa
 import gradio as gr
 
-def img_to_audio(image, time=3.0, rate=22050, n_fft=1024, n_iter=64, hop_length=512, contrast_stretch=False, hist_equalize=False):
+def img_to_audio(image, time=3.0, rate=22050, n_fft=1024, n_iter=64, hop_length=512, contrast_stretch=False, hist_equalize=False, improve_reconstruction=False):
     # load image
     img = Image.fromarray(image).convert("L")
     
@@ -19,30 +19,31 @@ def img_to_audio(image, time=3.0, rate=22050, n_fft=1024, n_iter=64, hop_length=
     print(spec.shape)
     spec = np.interp(spec, (spec.min(), spec.max()), (-50, 30))
     spec = librosa.db_to_amplitude(spec)
-    audio = librosa.griffinlim(spec, n_iter=n_iter, hop_length=hop_length)
+    
+    if improve_reconstruction:
+        # Use advanced reconstruction method
+        audio = librosa.effects.preemphasis(librosa.feature.inverse.mel_to_audio(spec))
+    else:
+        # Use Griffin-Lim for reconstruction
+        audio = librosa.griffinlim(spec, n_iter=n_iter, hop_length=hop_length)
+    
     return (rate, audio)
 
 time = gr.Number(3.0, label="audio time")
 image = gr.Image(label="image to sonify")
 n_fft = gr.Number(1024, label="n_fft")
-
-# hop_length is a parameter used in the img_to_audio function to determine the number of samples between successive frames of a spectrogram.
-# It is used to control the trade-off between time and frequency resolution in the spectrogram.
 hop_length = gr.Number(512, label="hop_length")
-
-# contrast_stretch refers to a technique that stretches the contrast of the image to improve its dynamic range. 
-# This can help to bring out details in the image that might otherwise be difficult to see.
 contrast_stretch = gr.Checkbox(False, label="Apply contrast stretching")
-
-# hist_equalize refers to a technique that equalizes the histogram of the image,
-# which can help to bring out details in the image that might otherwise be difficult to see.
 hist_equalize = gr.Checkbox(False, label="Apply histogram equalization")
+improve_reconstruction = gr.Checkbox(False, label="Improve Griffin-Lim Reconstruction")
 
-def main(image, time, n_fft, hop_length, contrast_stretch, hist_equalize):
-    return img_to_audio(image, time=time, n_fft=int(n_fft), hop_length=int(hop_length), contrast_stretch=contrast_stretch, hist_equalize=hist_equalize)
+def main(image, time, n_fft, hop_length, contrast_stretch, hist_equalize, improve_reconstruction):
+    return img_to_audio(image, time=time, n_fft=int(n_fft), hop_length=int(hop_length),
+                        contrast_stretch=contrast_stretch, hist_equalize=hist_equalize,
+                        improve_reconstruction=improve_reconstruction)
 
 desc = "Upload an image you would like to hear."
 
-interface = gr.Interface(fn=main, inputs=[image, time, n_fft, hop_length, contrast_stretch, hist_equalize], outputs="audio", title="Improved Image Sonification", description=desc)
+interface = gr.Interface(fn=main, inputs=[image, time, n_fft, hop_length, contrast_stretch, hist_equalize, improve_reconstruction], outputs="audio", title="Improved Image Sonification", description=desc)
 
 interface.launch()
